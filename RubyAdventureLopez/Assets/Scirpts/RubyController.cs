aaa248100425b2633b06376202748676a2fba9d0
Christopher Lopez <145504732+Lunar-ElHombre@users.noreply.github.com>
2023-12-01 04:01:54 -0800

    animations
    added

--- RubyAdventureLopez/Assets/Scirpts/RubyController.cs
+++ RubyAdventureLopez/Assets/Scirpts/RubyController.cs
@@ -5,7 +5,7 @@ public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.5f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -16,11 +16,15 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
 
     }
@@ -30,6 +34,17 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) ||!Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if(isInvincible)
         {
@@ -43,7 +58,7 @@ public class RubyController : MonoBehaviour
 
 
     }
-    private void FixedUpdate()
+     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
@@ -57,14 +72,16 @@ public class RubyController : MonoBehaviour
     {
         if(amount < 0)
         {
-            return;
+            animator.SetTrigger("Hit");
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
         }
-        isInvincible = true;
-        invincibleTimer = timeInvincible;
-
-        
-        
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+               
+                currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 
